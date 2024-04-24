f6436803689d68f30a49b6f4a30ca7f92d193820
s890214 <sp8902142008@126.com>
2024-04-24 09:45:12 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -3,17 +3,19 @@
 
 
 # 1-添加ShadowSocksR Plus+插件
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # 2-添加PowerOff关机插件
-git clone https://github.com/TraderWukong/luci-app-poweroff.git package/luci-app-poweroff
+# git clone https://github.com/TraderWukong/luci-app-poweroff.git package/luci-app-poweroff
 
 # 3-添加Opentomcat主题
-git clone https://github.com/TraderWukong/luci-theme-opentomcat.git package/luci-theme-opentomcat
+# git clone https://github.com/TraderWukong/luci-theme-opentomcat.git package/luci-theme-opentomcat
 
 # 4-添加OpenClash插件
 sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
 
 # 5-添加PassWall插件
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+# echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+# echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+
+echo 'src-git lucky https://github.com/gdy666/luci-app-lucky' >>feeds.conf.default
